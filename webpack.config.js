f6fd6d0e41e61ac1691e22628562bd04030476bb
Dmitry <RadionovDmitry93@mail.ru>
2022-04-27 12:29:13 +0500

    Удаляет правило css в конфигурации webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,6 @@ module.exports = {
           test: /\.js$/,
           exclude: /(node_modules)/,
           use: ['babel-loader']
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
         }
     ]
   }
